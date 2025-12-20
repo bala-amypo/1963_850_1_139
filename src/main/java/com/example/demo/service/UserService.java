a99a5d2bb5fa00e2dcc0de5f1bac8609bf019dcc
bala-amypo <bala@amypo.in>
2025-12-20 18:38:29 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,9 +4,6 @@ import com.example.demo.entity.User;
 import java.util.Optional;
 
 public interface UserService {
-    // Used for Rule 6.1: Register and encrypt password
     User registerUser(User user);
-    
-    // Used for Rule 8.1: Find user for login/JWT
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
