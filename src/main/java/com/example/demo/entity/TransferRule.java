e01ff2a69e9c06730cb538f6e55c7be4f6b94ae2
bala-amypo <bala@amypo.in>
2025-12-20 14:41:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferRule.java
+++ src/main/java/com/example/demo/entity/TransferRule.java
@@ -1,5 +1,25 @@
 package com.example.demo.entity;
 
-public class TransferRule{
-    
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "transfer_rules")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class TransferRule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "source_university_id")
+    private University sourceUniversity;
+
+    @ManyToOne
+    @JoinColumn(name = "target_university_id")
+    private University targetUniversity;
+
+    private Double minimumOverlapPercentage;
+    private Integer creditHourTolerance;
+    private Boolean active = true;
 }
