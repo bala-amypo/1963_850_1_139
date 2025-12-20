07090430a98af33d9825a50f9cc8dd9229292e0d
bala-amypo <bala@amypo.in>
2025-12-20 15:39:55 +0000

    User commit

--- src/main/java/com/example/demo/service/UniversityService.java
+++ src/main/java/com/example/demo/service/UniversityService.java
@@ -4,12 +4,11 @@ import com.example.demo.entity.University;
 import java.util.List;
 
 public interface UniversityService {
-    // Rule 6.2: Create universities
+    // Rule 6.2: Create a new university
     University createUniversity(University university);
-
-    // Rule 6.2: Fetch by ID (must handle not-found)
+    
+    // Rule 6.2: Find university by ID (handles "not found")
     University getById(Long id);
-
-    // Optional but helpful: List all universities
+    
     List<University> getAllUniversities();
 }
