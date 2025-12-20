eed07b31591b48e711045a28012391a9b3b89fc1
bala-amypo <bala@amypo.in>
2025-12-20 14:39:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -1,5 +1,22 @@
 package com.example.demo.entity;
 
-public class University{
-    
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "universities")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class University {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String accreditationLevel;
+    private String country;
+
+    @Column(nullable = false)
+    private Boolean active = true; // Defaults to true
 }
