c88b6c2cc678b1ed9b2db73f928e896525fa642f
bala-amypo <bala@amypo.in>
2025-12-20 16:43:18 +0000

    User commit

--- src/main/java/com/example/demo/controller/UniversityController.java
+++ src/main/java/com/example/demo/controller/UniversityController.java
@@ -1,5 +1,33 @@
 package com.example.demo.controller;
 
-public class UniversityController{
-    
+import com.example.demo.entity.University;
+import com.example.demo.service.UniversityService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/universities")
+public class UniversityController {
+
+    private final UniversityService universityService;
+
+    public UniversityController(UniversityService universityService) {
+        this.universityService = universityService;
+    }
+
+    @PostMapping
+    public ResponseEntity<University> create(@RequestBody University university) {
+        return ResponseEntity.ok(universityService.createUniversity(university));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<University> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(universityService.getById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<University>> getAll() {
+        return ResponseEntity.ok(universityService.getAllUniversities());
+    }
 }
