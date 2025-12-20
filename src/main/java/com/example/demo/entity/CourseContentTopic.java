f673b2ca4d97bc08f71d4f7214aaf4551bdf6a33
bala-amypo <bala@amypo.in>
2025-12-20 14:40:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -1,5 +1,22 @@
 package com.example.demo.entity;
 
-public class CourseContentTopic{
-    
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "course_content_topics")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class CourseContentTopic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Column(nullable = false)
+    private String topicName;
+
+    private Double weightPercentage;
 }
