204071cf50b5d7511abb434cec122a92e8bded2b
bala-amypo <bala@amypo.in>
2025-12-20 14:39:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -1,5 +1,26 @@
 package com.example.demo.entity;
 
-public class Course{
-    
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "courses", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"university_id", "courseCode"}) // Unique constraint
+})
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "university_id", nullable = false)
+    private University university;
+
+    private String courseCode;
+    private String courseName;
+    private Integer creditHours;
+    private String description;
+    private String department;
+    private Boolean active = true;
 }
