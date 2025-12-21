99f6439af3c4a0c3272095a8bac6337a596b3c06
bala-amypo <bala@amypo.in>
2025-12-21 09:51:24 +0000

    User commit

--- src/main/java/com/example/demo/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message); // Message will contain keywords like "exists", "> 0", "0-100"
-    }
-}
--- src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.dto.UserDTO;
-import com.example.demo.entity.User;
-
-public interface UserService {
-    User registerUser(UserDTO userDto);
-}
