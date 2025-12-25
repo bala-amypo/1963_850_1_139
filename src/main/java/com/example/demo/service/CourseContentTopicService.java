54696aa4be8cf402273023ef4c14a5f57647620b
bala-amypo <bala@amypo.in>
2025-12-25 06:52:55 +0000

    User commit

--- src/main/java/com/example/demo/service/CourseContentTopicService.java
+++ src/main/java/com/example/demo/service/CourseContentTopicService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.CourseContentTopic;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface CourseContentTopicService {
 
     CourseContentTopic createTopic(CourseContentTopic topic);
